932bf55ac186edaee8b0ba25f30bac54b867b88d
goldenway <goldenway777@gmail.com>
2024-03-06 23:12:40 +0100

    implement password strength testing logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+const MIN_LENGTH = 8;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'password-strength-app';
+  passwordStrength: string = '';
+
+  checkPasswordStrength(event: any): void {
+    const password = (event.target as HTMLInputElement).value;
+
+    if (!password || password.length === 0) {
+      this.passwordStrength = 'empty';
+      return;
+    }
+    
+    if (password.length < MIN_LENGTH) {
+      this.passwordStrength = 'short';
+      return;
+    }
+    
+    if (/^[a-zA-Z]+$|^\d+$|^[!@#$%^&*(),.?":{}|<>]+$/.test(password)) {
+      this.passwordStrength = 'easy';
+    } else if (/[a-zA-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      this.passwordStrength = 'strong';
+    } else {
+      this.passwordStrength = 'medium';
+    }
+  }
 }
